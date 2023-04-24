0f286e0edb6551807b56587f71c580286e5de047
Vinicius Leal da Silva <Vinicius.leal@divinaprovidencia.org.br>
2023-04-24 13:06:54 -0300

    L

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,28 @@
-
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { and } from '@angular/router/src/utils/collection';
-import { SwPush, SwUpdate } from '@angular/service-worker';
-
-
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { and } from "@angular/router/esm5/src/utils/collection";
+import { SwPush, SwUpdate } from "@angular/service-worker";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-
-  VAPID_PUBLIC_KEY='BHmZtplhinGo5mIfNNWmwhSpNAF2YZaHRjQjtDaBwTDej5uwwEobMVUsxe7rfCbCtqShwRuqezsOW5DDNs7DuiM'
-  constructor(
-    private router: Router
-    
-  ) {
-
-  }
+  VAPID_PUBLIC_KEY =
+    "BHmZtplhinGo5mIfNNWmwhSpNAF2YZaHRjQjtDaBwTDej5uwwEobMVUsxe7rfCbCtqShwRuqezsOW5DDNs7DuiM";
+  constructor(private router: Router) {}
 
   exibindoNavbar() {
-
-    if( this.router.url !== '/login' && this.router.url !== '/forgot'
-    && this.router.url !== '/'  && (this.router.url.includes('/appaplic') === false)) {
-
+    if (
+      this.router.url !== "/login" &&
+      this.router.url !== "/forgot" &&
+      this.router.url !== "/" &&
+      this.router.url.includes("/appaplic") === false
+    ) {
       return true;
-
     }
 
     return false;
   }
-
- 
-
 }
--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,42 +1,40 @@
-import { AuthService } from './../../seguranca/auth.service';
-import { Router } from '@angular/router';
-import { NgForm } from '@angular/forms/src/directives';
-import { Component, OnInit } from '@angular/core';
-import { MessageService } from 'primeng/components/common/messageservice';
-
+import { AuthService } from "./../../seguranca/auth.service";
+import { Router } from "@angular/router";
+import { NgForm } from "@angular/forms/esm5/src/directives";
+import { Component, OnInit } from "@angular/core";
+import { MessageService } from "primeng/components/common/messageservice";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+    private messageService: MessageService
+  ) {}
 
-  constructor(private router: Router,
-  private auth: AuthService,
-  private messageService: MessageService,
-) { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   login(form: NgForm) {
-      this.auth.login(form.value.usuario, form.value.senha)
+    this.auth
+      .login(form.value.usuario, form.value.senha)
       .then(() => {
-       if( this.auth.temPermissao('ROLE_CRM')) {
-         console.log(this.auth.temPermissao('ROLE_CRM'));
-        this.router.navigate(['/crm']);
-       }  if ( this.auth.temPermissao('ROLE_CONSULTA')) {
-
-         this.router.navigate(['/faleconosco']);
-       } if ( this.auth.temPermissao('ROLE_OC')) {
-         this.router.navigate(['/ocupacao']);
-      }
-      }).catch(erro => {
-       
-        this.messageService.add({ severity: 'error', detail: erro });
-
+        if (this.auth.temPermissao("ROLE_CRM")) {
+          console.log(this.auth.temPermissao("ROLE_CRM"));
+          this.router.navigate(["/crm"]);
+        }
+        if (this.auth.temPermissao("ROLE_CONSULTA")) {
+          this.router.navigate(["/faleconosco"]);
+        }
+        if (this.auth.temPermissao("ROLE_OC")) {
+          this.router.navigate(["/ocupacao"]);
+        }
+      })
+      .catch((erro) => {
+        this.messageService.add({ severity: "error", detail: erro });
       });
-
   }
 }
